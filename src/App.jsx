66cc7d6bc736b5f05a55fae7ace691b3fb68d6e5
withrifat <mdrifathassain97880@gmail.com>
2025-04-22 06:59:25 +0600

    add besic react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import SectionOne from './components/SectionOne';
+import SectionTwo from './components/SectionTwo';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SectionOne/>
+      <SectionTwo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,13 @@
+function Nav() {
+    return (
+      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+        <a className="mr-5 hover:text-gray-900">First Link</a>
+        <a className="mr-5 hover:text-gray-900">Second Link</a>
+        <a className="mr-5 hover:text-gray-900">Third Link</a>
+        <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+      </nav>
+    );
+  }
+  
+  export default Nav;
+  
